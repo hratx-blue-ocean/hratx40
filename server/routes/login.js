b04af9f9c54d68381d36f43bfb8b7e34f85f12aa
Drew <drew.distefano@me.com>
2019-05-06 17:25:22 -0500

    created route, sans query

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { validateLogin } = require('../../db/db.js')
 
 //for frontend, pass in params as `{params: {username: <input>}}`
-router.get('/logins', (req, res)=>{
+router.get('/logins', (req, res) => {
   let username = req.query.username;
-  validateLogin(username, (err, data)=>{
+  validateLogin(username, (err, data) => {
     if (err){
       console.log('Server erorr: Unable to run validateLogin script')
     }
