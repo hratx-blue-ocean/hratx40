5e60ac0dea3d82eaf8846cc2e1f6419c3be71185
Drew <drew.distefano@me.com>
2019-05-06 17:17:20 -0500

    created basic route, helper function not started

---
+++ server/routes/login.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { validateLogin } = require('../../db/db.js')
+
+//for frontend, pass in params as `{params: {username: <input>}}`
+router.get('/logins', (req, res)=>{
+  let username = req.query.username;
+  validateLogin(username, (err, data)=>{
+    if (err){
+      console.log('Server erorr: Unable to run validateLogin script')
+    }
+    else {
+      res.status(200).send(data);
+    }
+  })
+});
+
+module.exports = { logins };
