7a5e7eb242723ffc54ba6f80360521e43434d96c
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-05-06 17:06:21 -0500

    added .sql file to intuitively seed topics table

--- server/routes/seedTopics.js
+++ server/routes/seedTopics.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const axios = require('axios');
 const db = require('../../db/db');
+const fs = require('fs');
 
 router.get('/', (req, res) => {
     axios.get('https://api.data.charitynavigator.org/v2/Categories?app_id=ad60fc1b&app_key=d905962cb8b5dbd0f55db21e8c8ba7fc')
     .then(data => {
         for (let i = 0; i < data.data.length; i++) {
             for (let j = 0; j < data.data[i].causes.length; j++) {
+                // fs.appendFile('../db/seedTopics.sql',`insert into topics(topic_name,topic_imageUrl,website_url) values ('${data.data[i].causes[j].causeName}','${data.data[i].causes[j].image}','${data.data[i].causes[j].charityNavigatorURL}');\n`, (err, data) => {
+                //     if (err) {
+                //         res.end()
+                //     } 
+                // });
                 db.insertTopic(data.data[i].causes[j], (err, result) => {
                     if (err) {
                         res.end()
