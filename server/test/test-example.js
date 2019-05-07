8271d08aab83a8305bb58cab6a30ca041b86beb1
JonathonLLopez <jonathonllopez@gmail.com>
2019-05-07 13:39:08 -0500

    seperated out tests for test-example for example purposes

--- server/test/test-example.js
+++ server/test/test-example.js
@@ -1,24 +1,37 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const should = chai.should();
+// const should = chai.should();
 // Tests can also be written with 'expect' rather than 'should' if desired
-// const expect = chai.expect;
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
 describe('GET example', () => {
-  it('it should GET sample data', () => {
-    chai
-      .request(`http://localhost:8000`)
+  it('it should GET sample data', (done) => {
+    chai.request(`http://localhost:8000`)
       .get('/api/example')
-      .then((err, res) => {
-        should.not.exist(err);
-        should.exist(res);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
+      .end((err, res) => {
+        expect(res).to.exist;
         done();
       })
-      .catch(err => err);
   });
+  it(`it should send a status code of 200`, (done) => {
+    chai.request(`http://localhost:8000`)
+      .get(`/api/example`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      })
+  })
+  it(`it should have a res.body of an object`, (done) => {
+    chai.request(`http://localhost:8000`)
+      .get(`/api/example`)
+      .end((err, res) => {
+        expect(res.body).to.be.a(`object`);
+        done();
+      })
+  })
 });
+
+
