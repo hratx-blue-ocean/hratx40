c9523fb392b598ef715e9c70cd5f089927e898ee
Nicholas Miron <45276017+NicholasMiron@users.noreply.github.com>
2019-05-07 15:57:01 -0500

    Ticket 56, 9 Landing Page / Hero Banner (#29)
    * Added landing page to app.js
    
    * made the herobanner
    
    * Added style sheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import fetch from 'node-fetch';
+import LandingPage from './Components/LandingPage.js'
 // import './App.css';
 import Modal from './Components/Modal.js';
 
@@ -55,6 +56,7 @@ export default class App extends Component {
   render() {
     return (
       <>
+        <LandingPage topics={[]}/>
         <h1>Welcome to Blue Ocean!</h1>
         <button name="volunteer" onClick={(event) => this.toggleModal(event)}>Press Me!</button>
         <Modal modalType={this.state.modalType} isOpen={this.state.isOpen} toggleOpen={this.toggleModal}/>
