822866bf910175e1786ced6ca590999c342aba78
Drew DiStefano <46331903+drewdistefano@users.noreply.github.com>
2019-05-07 16:53:27 -0500

    Ticket #30 (create route login) https://trello.com/c/Gr2Nj8Xu (#30)
    * created basic route, helper function not started
    
    * created route, sans query
    
    * implemented passwordHash
    
    * commented out test
    
    * commented out test
    
    * moved password hash to server package.json
    
    * resolved conflicts with module exports

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,9 @@ const charity = require('./charity');
 const seedTopics = require('./seedTopics');
 const search = require('./search');
 const addFavorite = require('./favorites');
+const login = require('./login')
 const news = require("./newsfeed");
 const deleteFavorite = require('./deleteFave');
 const getAllTopics = require('./getAllTopics');
 
-module.exports = { example, seedTopics, search, addFavorite, deleteFavorite, charity, getAllTopics, news }
+module.exports = { example, seedTopics, search, addFavorite, deleteFavorite, charity, getAllTopics, news, login }
