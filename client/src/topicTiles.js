8d88782ab52707a99831372be650df7e46f8fd57
matthew <matt.feinberg@gmail.com>
2019-05-06 20:01:05 -0500

    end of day merge, still researchingmaterial ui grid

--- client/src/topicTiles.js
+++ client/src/topicTiles.js
@@ -1,8 +1,13 @@
 import React from "react";
 import tile from "..src/topicTile";
+import Grid from "@material-ui/core/Grid";
+
+// const topicTiles = props.topicTiles.map(tile => {});
 
 const TopicTiles = props => {
-  const topicTiles = props.topicTiles.map(tile => {});
+  <div>
+    <Grid />;
+  </div>;
 };
 
 export default TopicTiles;
