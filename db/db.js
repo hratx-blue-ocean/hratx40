ff8bab451ab16f9ad60de6f173c87a9518dc6beb
Nicholas Miron <45276017+NicholasMiron@users.noreply.github.com>
2019-05-06 20:21:59 -0500

    EOD Day 1 (#13)
    * Fixed launch scripts
    
    * Made client run in background
    
    * Ticket #21. Database schema.  (#5)
    
    * completed database schema
    
    * updated database schema to not drop tables, as it is redundant
    
    * updated schema to allow only one user_id, topic_id combo in users_topics table and removed username from users
    
    * updated schema to include username
    
    * Ticket # 36. Connect db server (#6)
    
    * added db file with test query that connects db to query, just require file in route file
    
    * added pg to package.json
    
    * Updated db configuration to all be process.env variables
    
    * updated with most recent package-locks
    
    * Ticket #46: Installed Material-UI. Now renders Material-UI components and SVG Icons. (#7)
    
    Added material UI library
    
    * Changed test script to run all tests (#10)
    
    Main test script modified to run client and server build tests in one line. CI should agree with this.
    
    * Ticket # 37. Populate topics (#11) Passing tests, environment variables set.
    
    * completed route for seeding topics data, in addition, added axios to package.json and updated the dotenv configuration to be more correct
    
    * added .sql file to intuitively seed topics table
    
    * added comments to seedTopics route for legacy code developers
    
    * updated api call to use process.env instead of sensitive information
    
    * Ticket # 32. Search route (#12)
    
    * added in route for search, and db query for topics
    
    * added route for search, this allows search for topics. In addition, I've created a db query to facilitate search, and one update to the schema (making topic_name unique). this will make it so that the table cannot be seeded multiple times with the same data
    
    * updated search route to use fields instead of rows, fixed bug
    
    * added a test for search, it passes

---
+++ db/db.js
@@ -0,0 +1,46 @@
+const Pool = require('pg').Pool;
+require('dotenv').config({path: '../.env'});
+
+//.env file must be inside the server folder
+const pool = new Pool({
+    user: process.env.DB_USER, //needs to be changed to work in your instance
+    host: process.env.DB_HOST, //localhost for most
+    database: process.env.DB_NAME, //blueocean is working name, open to adjustment
+    password: process.env.DB_PASSWORD, //password
+    port: process.env.DB_PORT //port, please put 5432 unless you have need to use another
+});
+
+
+const getAll = (cb) => {
+    pool.query('select * from users', (err, data) => {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, data);
+        }
+    });
+};
+
+const insertTopic = (causeObj, cb) => {
+    pool.query(`insert into topics(topic_name,topic_imageUrl,website_url) values 
+    ('${causeObj.causeName}','${causeObj.image}','${causeObj.charityNavigatorURL}')`, (err, data) => {
+        if (err) {
+            cb(err)
+        } else {
+            cb(null,data)
+        }
+    });
+};
+
+const getTopic = (topicName, cb) => {
+    topicName = topicName.replace(`'`,`''`)
+    pool.query(`select * from topics where lower(topic_name) like lower('%${topicName}%')`, (err, data) => {
+        if (err) {
+            cb(err);
+        } else {
+            cb(null,data);
+        }
+    });
+}
+
+module.exports = { getAll, getTopic, insertTopic }
