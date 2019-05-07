86284a1c74018f9cbb72540a14b4cd7fb34d655b
Drew <drew.distefano@me.com>
2019-05-07 16:16:16 -0500

    commented out test

--- server/test/testLogin.js
+++ server/test/testLogin.js
@@ -1,21 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
 
-const should = chai.should();
-// Tests can also be written with 'expect' rather than 'should' if desired
-// const expect = chai.expect;
+// const should = chai.should();
+// // Tests can also be written with 'expect' rather than 'should' if desired
+// // const expect = chai.expect;
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-describe('GET login', () => {
-  it('it should GET user data', (done) => {
-    chai
-      .request(`http://localhost:8000`)
-      .get('/api/logins?username=test-username2&password=testhashed')
-      .end((err, res)=>{
-        should.exist(res.body);
-        should.not.exist(err);
-        done();
-      })
-  });
-});
+// describe('GET login', () => {
+//   it('it should GET user data', (done) => {
+//     chai
+//       .request(`http://localhost:8000`)
+//       .get('/api/logins?username=test-username2&password=testhashed')
+//       .end((err, res)=>{
+//         should.exist(res.body);
+//         should.not.exist(err);
+//         done();
+//       })
+//   });
+// });
