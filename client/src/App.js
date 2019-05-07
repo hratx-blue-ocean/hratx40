78fd90792a7f13392b706fb30abc678f34c26aec
Jonathan <masteraikeus@gmail.com>
2019-05-07 12:01:08 -0500

    completed basic modal outline, currently using inline styling, since it won't talk to .css

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import fetch from 'node-fetch';
-// import './App.css';
+import './App.css';
 import Modal from './Modal.js';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seaCreatures: []
+      seaCreatures: [],
+      isOpen: false,
+      modalType: ""
     };
     this.api = `http://localhost:8000/api/example`;
+    this.toggleModal = this.toggleModal.bind(this);
   }
   componentDidMount() {
     fetch(this.api)
@@ -19,11 +22,29 @@ export default class App extends Component {
       });
   }
 
+  // Toggles if the Modal is open or closed
+  // upon open, sets the modalType using the element's name
+  toggleModal (event) {
+    event.preventDefault();
+    let open = !this.state.isOpen;
+    if(this.open) {
+      let name = event.target.name;
+      this.setState({ 
+        isOpen: open, 
+        modalType: name
+      });
+    } else {
+      this.setState({ 
+        isOpen: open 
+      });
+    }
+  }
+
   render() {
     return (
       <>
         <h1>Welcome to Blue Ocean!</h1>
-        <Modal modalType={"login"} isOpen={true}/>
+        <Modal modalType={"login"} isOpen={this.state.isOpen} toggleOpen={this.toggleModal}/>
         <ul>
           {this.state.seaCreatures.map((creature, index) => (
             <li key={index}>{creature}</li>
--- client/src/Modal.js
+++ client/src/Modal.js
@@ -3,50 +3,116 @@ import Modal from '@material-ui/core/Modal';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
+// Import child modals
 const ModalDiv = (props) => {
   if (props.modalType === "login") {
     return (
       <Modal open={props.isOpen}>
-        <div>
-          {/* Import login modal here */}
-          <Grid>
-            <Paper>
-              Login
+        <Grid container
+              justify="center"
+              style={{marginTop: "20vh"}}>
+          <svg onClick={(event) => {props.toggleOpen(event)}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          <Grid item xs={9}
+                styles={{justify: "center"}}>
+            <Paper 
+              className="parent-modal"
+              style={{height:"400px"}}
+              square={true}>
+              {/* Import login modal here */}
             </Paper>
           </Grid>
-        </div>
+        </Grid>
       </Modal>
     )
   } else if (props.modalType === "signup") {
     return (
       <Modal open={props.isOpen}>
-        <div>
-          {/* Import signup modal here */}
-        </div>
+        <Grid container
+              justify="center"
+              style={{marginTop: "20vh"}}>
+          <svg onClick={(event) => {props.toggleOpen(event)}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          <Grid item xs={9}
+                styles={{justify: "center"}}>
+            <Paper 
+              className="parent-modal"
+              style={{height:"400px"}}
+              square={true}>
+              {/* Import signup modal here */}
+            </Paper>
+          </Grid>
+        </Grid>
       </Modal>
     )
   } else if (props.modalType === "donate") {
     return (
       <Modal open={props.isOpen}>
-        <div>
-          {/* Import donate modal here */}
-        </div>
+        <Grid container
+              justify="center"
+              style={{marginTop: "20vh"}}>
+          <svg onClick={(event) => {props.toggleOpen(event)}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          <Grid item xs={9}
+                styles={{justify: "center"}}>
+            <Paper 
+              className="parent-modal"
+              style={{height:"400px"}}
+              square={true}>
+              {/* Import donate modal here */}
+            </Paper>
+          </Grid>
+        </Grid>
       </Modal>
     )
   } else if (props.modalType === "volunteer") {
     return (
       <Modal open={props.isOpen}>
-        <div>
-          {/* Import volunteer modal here */}
-        </div>
+        <Grid container
+              justify="center"
+              style={{marginTop: "20vh"}}>
+          <svg onClick={(event) => {props.toggleOpen(event)}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          <Grid item xs={9}
+                styles={{justify: "center"}}>
+            <Paper 
+              className="parent-modal"
+              style={{height:"400px"}}
+              square={true}>
+              {/* Import volunteer modal here */}
+            </Paper>
+          </Grid>
+        </Grid>
       </Modal>
     )
   } else if (props.modalType === "charities") {
     return (
       <Modal open={props.isOpen}>
-        <div>
-          {/* Import charity modal here */}
-        </div>
+        <Grid container
+              justify="center"
+              style={{marginTop: "20vh"}}>
+          <svg onClick={(event) => {props.toggleOpen(event)}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+          <Grid item xs={9}
+                styles={{justify: "center"}}>
+            <Paper 
+              className="parent-modal"
+              style={{height:"400px"}}
+              square={true}>
+              {/* Import charities modal here */}
+            </Paper>
+          </Grid>
+        </Grid>
       </Modal>
     )
   }
