c8ea30204513a6dc7d73e6d1de26126c87dc5e48
matthew <matt.feinberg@gmail.com>
2019-05-07 11:46:29 -0500

    researching Grid examples

--- client/src/topicTiles.js
+++ client/src/topicTiles.js
@@ -1,8 +1,17 @@
 import React from "react";
-import tile from "..src/topicTile";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import pink from '@material-ui/core/colors/pink';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const styles = theme => ({
   root: {
@@ -21,6 +30,9 @@ const styles = theme => ({
 // const topicTiles = props.topicTiles.map(tile => {});
 
 const TopicTiles = props => {
+  <Grid container className = {topicTilesGrid} spacing={16}>
+  <Grid item xs
+  </Grid>
   <div onClick={props.handleClick}>
     <Grid />;
   </div>;
