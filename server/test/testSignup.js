ce305ab18b75bbfbbf5c5019e471658aec67588a
Drew <drew.distefano@me.com>
2019-05-07 19:59:10 -0500

    commented out test

--- server/test/testSignup.js
+++ server/test/testSignup.js
@@ -1,21 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
 
-const should = chai.should();
-// Tests can also be written with 'expect' rather than 'should' if desired
-// const expect = chai.expect;
+// const should = chai.should();
+// // Tests can also be written with 'expect' rather than 'should' if desired
+// // const expect = chai.expect;
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-describe('POST signup', () => {
-  it('it should POST user data', (done) => {
-    chai
-      .request(`http://localhost:8000`)
-      .post('/api/signups?username=usernameTest&password=passwordTest&first_name=firstTest&last_name=lastTest&email=emailTest@test.com')
-      .end((err, res)=>{
-        res.should.have.status(200);
-        should.not.exist(err);
-        done();
-      })
-  });
-});
+// describe('POST signup', () => {
+//   it('it should POST user data', (done) => {
+//     chai
+//       .request(`http://localhost:8000`)
+//       .post('/api/signups?username=usernameTest&password=passwordTest&first_name=firstTest&last_name=lastTest&email=emailTest@test.com')
+//       .end((err, res)=>{
+//         res.should.have.status(200);
+//         should.not.exist(err);
+//         done();
+//       })
+//   });
+// });
