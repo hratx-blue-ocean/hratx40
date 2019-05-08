f8dbb3f21e7cc0363504a65f00ea146cfbfbe6af
David Castillo <d.v.castillo37@gmail.com>
2019-05-08 10:55:29 -0500

    wrapping up final changes on newsfeed

--- client/src/Components/Newsfeed.js
+++ client/src/Components/Newsfeed.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import axios from "axios";
 
@@ -9,34 +9,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: [
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" },
-        { urlToImage: "" }
-      ]
+      articles: []
     };
   }
 
   componentDidMount() {
     axios
       .get(
-        `https://newsapi.org/v2/everything?q="hunger"&sortBy=popularity&language=en&apiKey=23a22828c7c44f4b85cdcacbf8876f82`
+        `https://newsapi.org/v2/everything?q=(hunger AND charity)&sortBy=popularity&language=en&apiKey=23a22828c7c44f4b85cdcacbf8876f82`
       )
       .then(data => {
-        console.log(data.data.articles);
+        // console.log(data.data.articles);
         this.setState({
           articles: data.data.articles
         });
       })
       .catch(err => {
-        console.error(`ERROR: ${err}`);
+        throw err;
       });
   }
 
@@ -44,14 +33,20 @@ class App extends React.Component {
     return (
       <div className="App">
       { this.state.articles.map((article)=>{ 
-        <Card>
-          <CardHeader>
-            <h1>{article.title}</h1>
-          </CardHeader>
-          <CardContent>
-            <h2>{article.title}</h2>
-          </CardContent>
-        </Card>  
+        return (
+          <a href={`${article.url}`} target="_blank" style={{color:"black", textDecoration:"none"}}>
+            <Card style={{display:"flex", flexDirection:"row", alignItems:"center", justify:"center", width:"100%"}}>
+                <CardContent style={{width:"50%"}}>
+                  <h2>{article.title}</h2>
+                </CardContent>
+                <CardMedia
+                  style={{width:"50%"}}
+                  component="img"
+                  image={article.urlToImage}
+                />
+            </Card>  
+          </a>
+        )
       })}
       </div>
     )
@@ -60,5 +55,6 @@ class App extends React.Component {
 
 
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+export default App;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import Newsfeed from './Components/Newsfeed.js'
 import './index.css';
 import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Newsfeed />, document.getElementById('root'));
