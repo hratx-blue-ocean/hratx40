104aeaad813223cb692a2071b0069386f04cfaab
Drew <dlockliear@gmail.com>
2019-05-09 14:42:22 -0500

    Conditionally rendering info, conctact, location

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import { faInfoCircle, faEnvelope, faMapMarkerAlt, faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
@@ -25,38 +29,107 @@ class Footer extends React.Component {
       location: false
     }
     
+    this.handleContact = this.handleContact.bind(this);
+    this.handleInfo = this.handleInfo.bind(this);
+    this.handleLocation = this.handleLocation.bind(this);
   }
+
+  checkInfo() {
+    if (this.state.info) {
+      return (
+        <Paper>
+          <h1>hratx40</h1>
+
+
+
+        </Paper>
+      )
+    }
+  }
+
+  checkContact() {
+    if (this.state.contact) {
+      return (
+        <div>
+          <p>Contact</p>
+        </div>
+      )
+    }
+  }
+
+  checkLocation() {
+    if (this.state.location) {
+      return (
+        <div>
+          <p>location</p>
+        </div>
+      )
+    }
+  }
+
+  handleInfo() {
+    if (this.state.info === true) {
+      this.setState({
+        info: false,
+        contact: false,
+        location: false
+      })
+    } else {
+      this.setState({
+        info: true,
+        contact: false,
+        location: false
+      })
+    }
+  }
+
+  handleContact() {
+    if (this.state.contact === true) {
+      this.setState({
+        contact: false,
+        location: false,
+        info: false
+      })
+    } else {
+      this.setState({
+        contact: true,
+        location: false,
+        info: false
+      })
+    }
+  }
+
+  handleLocation() {
+    if (this.state.location === true) {
+      this.setState({
+        contact: false,
+        info: false,
+        location: false
+      })
+    } else {
+      this.setState({
+        contact: false,
+        info: false,
+        location: true,
+      })
+    }
+  }
+  
 
   render() {
     if (this.state.icons === true) {
       return (
-        <div style={divStyle}>
-          <FontAwesomeIcon name='info' icon={faInfoCircle} size='2x'/>
-          <FontAwesomeIcon name='contact' icon={faEnvelope} size='2x'/>
-          <FontAwesomeIcon name='home' onClick={() => {this.props.footerPageChange()}} icon={faHome} size='2x'/>
-          <FontAwesomeIcon name='location' icon={faMapMarkerAlt} size='2x'/>
-          <FontAwesomeIcon onClick={() => {window.open('https://github.com/hratx-blue-ocean/hratx40',"_blank")}} icon={faGithub} size='2x'/>
-        </div>
-      )
-    }
-    if (this.state.info === true) {
-      return (
         <div>
-          <h1>info</h1>
-        </div>
-      )
-    }
-    if (this.state.contact === true) {
-      return (
-        <div>
-          <h1>info</h1>
-        </div>
-      )
-    }
-    if (this.state.location === true) {
-      return (
-        <div>
-          <h1>info</h1>
+          <div style={divStyle}>
+            <FontAwesomeIcon name='info' icon={faInfoCircle} size='2x' onClick={() => {this.handleInfo()}}/>
+            <FontAwesomeIcon name='contact' icon={faEnvelope} size='2x' onClick={() => {this.handleContact()}}/>
+            <FontAwesomeIcon name='home' onClick={() => {this.props.footerPageChange()}} icon={faHome} size='2x'/>
+            <FontAwesomeIcon name='location' icon={faMapMarkerAlt} size='2x' onClick={() => {this.handleLocation()}}/>
+            <FontAwesomeIcon onClick={() => {window.open('https://github.com/hratx-blue-ocean/hratx40',"_blank")}} icon={faGithub} size='2x'/>
+          </div>
+          {this.checkInfo()}
+          {this.checkContact()}
+          {this.checkLocation()}
         </div>
       )
     }
