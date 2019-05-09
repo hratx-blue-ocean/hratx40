a89199b712059484db4994342589b585f90bbb77
Drew Lockliear <dlockliear@gmail.com>
2019-05-09 15:34:39 -0500

    Footer [#20] (#87)
    * Conditionally rendering info, conctact, location
    
    * Paper rendering below icons for info, contact, and location
    
    * Added styling to paper components
    
    * Added spacing to paper components
    
    * New Footer ;)

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import { faInfoCircle, faEnvelope, faMapMarkerAlt, faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
@@ -14,6 +18,10 @@ const divStyle = {
   opacity: '.90'
 }
 
+const paperStyle = {
+  textAlign: 'center'
+}
+
 class Footer extends React.Component {
   constructor(props) {
     super(props);
@@ -25,38 +33,134 @@ class Footer extends React.Component {
       location: false
     }
     
+    this.handleContact = this.handleContact.bind(this);
+    this.handleInfo = this.handleInfo.bind(this);
+    this.handleLocation = this.handleLocation.bind(this);
+  }
+
+  checkInfo() {
+    if (this.state.info) {
+      return (
+        <Paper>
+          <div>
+            <br />
+            <h2 style={paperStyle}>HRATX40</h2>
+            <ul>
+              <li>Austin Anderson</li>
+              <li>David Castillo</li>
+              <li>Drew DiStefano</li>
+              <li>Drew Lockliear</li>
+              <li>Jarrod Miller</li>
+              <li>Jay Kannaiyan</li>
+              <li>Jonathan Becker</li>
+              <li>Jonathon Lopez</li>
+              <li>Maggie Gray</li>
+              <li>Mathew Feinberg</li>
+              <li>Nicholas Miron</li>
+              <li>Ryan Ferrer</li>
+            </ul>
+            <br />
+          </div>
+        </Paper>
+      )
+    }
+  }
+
+  checkContact() {
+    if (this.state.contact) {
+      return (
+        <Paper>
+          <div>
+            <br />
+            <h2 style={paperStyle}>Contact</h2>
+            <h4 style={paperStyle}>makeaaripl@gmail.com</h4>
+            <br />
+          </div>
+        </Paper>
+      )
+    }
+  }
+
+  checkLocation() {
+    if (this.state.location) {
+      return (
+        <Paper>
+          <div>
+            <br />
+            <h2 style={paperStyle}>Location</h2>
+            <h4 style={paperStyle}>Autstin, TX</h4>
+            <br />
+          </div>
+        </Paper>
+      )
+    }
+  }
+
+  handleInfo() {
+    if (this.state.info === true) {
+      this.setState({
+        info: false,
+        contact: false,
+        location: false
+      })
+    } else {
+      this.setState({
+        info: true,
+        contact: false,
+        location: false
+      })
+      this.scrollToBottom()
+    }
+  }
+
+  handleContact() {
+    if (this.state.contact === true) {
+      this.setState({
+        contact: false,
+        location: false,
+        info: false
+      })
+    } else {
+      this.setState({
+        contact: true,
+        location: false,
+        info: false
+      })
+      this.scrollToBottom()
+    }
+  }
+
+  handleLocation() {
+    if (this.state.location === true) {
+      this.setState({
+        contact: false,
+        info: false,
+        location: false
+      })
+    } else {
+      this.setState({
+        contact: false,
+        info: false,
+        location: true,
+      })
+      this.scrollToBottom();
+    }
   }
 
   render() {
     if (this.state.icons === true) {
       return (
-        <div style={divStyle}>
-          <FontAwesomeIcon name='info' icon={faInfoCircle} size='2x'/>
-          <FontAwesomeIcon name='contact' icon={faEnvelope} size='2x'/>
-          <FontAwesomeIcon name='home' onClick={() => {this.props.footerPageChange()}} icon={faHome} size='2x'/>
-          <FontAwesomeIcon name='location' icon={faMapMarkerAlt} size='2x'/>
-          <FontAwesomeIcon onClick={() => {window.open('https://github.com/hratx-blue-ocean/hratx40',"_blank")}} icon={faGithub} size='2x'/>
-        </div>
-      )
-    }
-    if (this.state.info === true) {
-      return (
         <div>
-          <h1>info</h1>
-        </div>
-      )
-    }
-    if (this.state.contact === true) {
-      return (
-        <div>
-          <h1>info</h1>
-        </div>
-      )
-    }
-    if (this.state.location === true) {
-      return (
-        <div>
-          <h1>info</h1>
+          <div style={divStyle}>
+            <FontAwesomeIcon name='info' icon={faInfoCircle} size='2x' onClick={() => {this.handleInfo()}}/>
+            <FontAwesomeIcon name='contact' icon={faEnvelope} size='2x' onClick={() => {this.handleContact()}}/>
+            <FontAwesomeIcon name='home' onClick={() => {this.props.footerPageChange()}} icon={faHome} size='2x'/>
+            <FontAwesomeIcon name='location' icon={faMapMarkerAlt} size='2x' onClick={() => {this.handleLocation()}}/>
+            <FontAwesomeIcon onClick={() => {window.open('https://github.com/hratx-blue-ocean/hratx40',"_blank")}} icon={faGithub} size='2x'/>
+          </div>
+          {this.checkInfo()}
+          {this.checkContact()}
+          {this.checkLocation()}
         </div>
       )
     }
