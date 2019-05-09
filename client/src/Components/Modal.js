360f4caf6e27f7f1367efa7a6d2c538b428dfc32
JonathonLLopez <jonathonllopez@gmail.com>
2019-05-09 15:09:29 -0500

    Have the modal toggling off when anywhere else on the screen is clicked

--- client/src/Components/Modal.js
+++ client/src/Components/Modal.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Login from './ChildModals/Login.js'
 import Signup from './ChildModals/Signup';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 // Import child modals
 const chooseChild = (type, toggleOpen, setLogin) => {
@@ -69,16 +70,22 @@ const chooseChild = (type, toggleOpen, setLogin) => {
   }
 }
 
+// const handleClickAway = () => {
+//   toggleOpen();
+// }
+
 const ModalDiv = (props) => {
   return (
     <Modal open={props.isOpen}>
       <Grid container
         justify="center"
         style={{ marginTop: "20vh", outline: "none" }}>
-        <svg onClick={(event) => { props.toggleOpen(event) }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-          <path d="M0 0h24v24H0z" fill="none" />
-        </svg>
+        <ClickAwayListener onClickAway={() => { props.toggleOpen(); }}>
+          <svg onClick={(event) => { props.toggleOpen(event) }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        </ClickAwayListener>
         <Grid item xs={6}
           styles={{ justify: "center" }}>
           <Paper
@@ -88,7 +95,7 @@ const ModalDiv = (props) => {
           </Paper>
         </Grid>
       </Grid>
-    </Modal>
+    </Modal >
   )
 }
 
